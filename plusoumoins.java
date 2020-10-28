75beecd7f5e3c873f07be532b2b60583e4503447
dany zaher <dany0.zaher@etu.u-pec.fr>
2020-10-28 17:36:41 +0100

    jeu plus ou moins avec max=100

---
+++ plusoumoins.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Random;
+class plusoumoins {
+	public static void main(String args[]) {
+		int n;
+		int reponse;
+		Random rand =new Random();
+		n = rand.nextInt(100);
+		boolean trouve = false;
+		Scanner scanner = new Scanner(System.in);
+		while (!trouve) {
+			System.out.println("entrez un nombre");
+			reponse = scanner.nextInt();
+			if (reponse == n) {
+				System.out.println("gg ez");
+				trouve = true;
+			} if (reponse > n)  {
+				System.out.println("c'est moins");
+			} if (reponse < n){
+				System.out.println("c'est plus");
+			}
+		}
+	}
+}
+			
+
