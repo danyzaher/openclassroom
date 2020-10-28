e6cefad1c1e94d8b6efe4ca4a4ec75fc200e0658
dany zaher <dany0.zaher@etu.u-pec.fr>
2020-10-28 17:17:15 +0100

    Ajout de demande de prénom

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 class helloworld {
 	public static void main(String args[]) {
-		System.out.println("Hello World");
+		Scanner scanner = new Scanner(System.in);
+		String nom = scanner.nextLine();
+		System.out.println("Hello World and you " + nom);
 	}
 }
